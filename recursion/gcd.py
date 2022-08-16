1fa5073b8d78786a1227da12335160f056d0a828
WORLDSAVER <pareekhridyansh@gmail.com>
2022-08-16 17:07:58 +0530

    Added gcd using recursion

---
+++ recursion/gcd.py
@@ -0,0 +1,16 @@
+# GCD USING EUCLIDEAN ALGORITHM
+
+def gcd(a, b):
+
+	if b==0:
+		return a  
+	else:
+		return gcd(b, a%b)   
+
+# TIME COMPLEXITY - O(log(min(a, b)))
+
+# EUCLIDEAN ALGORITHM 
+
+# EXAMPLES  
+# print(gcd(2, 5)) -> 1 
+# print(gcd(10, 2)) -> 2 
